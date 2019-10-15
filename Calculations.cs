af6b7c7c5b668c418e269086fbb6329366f38e59
Marcin Borysiak <marcinez24@gmail.com>
2019-10-15 18:45:37 +0200

    Added Calculations and ResultSet classes
    Calculations are supposed to run logarithm functions for multipleXs
    ResultSet is a class storing results for different methods used to calculate that X

---
+++ Calculations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NaturalLogarithm
+{
+    class Calculations
+    {
+        private double StartX, EndX;
+        private int XCount, N;
+        private Dictionary<double, double[]> ResultSet;
+
+        public Calculations(double startX, double endX, int xCount, int n)
+        {
+            StartX = startX;
+            EndX = endX;
+            XCount = xCount;
+            N = n;
+        }
+
+        public List<ResultSet> CalculateResults()
+        {
+            double x = StartX + (EndX / XCount);
+            while (x < EndX)
+            {
+                Logarithm l = new Logarithm(x, N);
+                l.CalculateNaiveFromEnd
+            }
+            
+        }
+
+    }
+}
---
+++ ResultSet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NaturalLogarithm
+{
+
+    class ResultSet
+    {
+        public double[] NaiveFromStart { get => NaiveFromStart; set => NaiveFromStart = value; }
+        public double[] NaiveFromEnd { get => NaiveFromEnd; set => NaiveFromEnd = value; }
+        public double[] SmartFromEnd { get => SmartFromEnd; set => SmartFromEnd = value; }
+        public double[] SmartFromStart { get => SmartFromStart; set => SmartFromStart = value; }
+        public double X { get => X; set => X = value; }
+
+        public ResultSet(double x, double[] naiveFromStart, double[] naiveFromEnd, double[] smartFromEnd, double[] smartFromStart)
+        {
+            X = x;
+            NaiveFromStart = naiveFromStart;
+            NaiveFromEnd = naiveFromEnd;
+            SmartFromEnd = smartFromEnd;
+            SmartFromStart = smartFromStart;
+        }
+
+    }
+}
